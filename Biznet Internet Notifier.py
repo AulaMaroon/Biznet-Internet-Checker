e5489baa039c5be55b4fd2695d51e8c738e9fa91
AulaMaroon <kadekfp@gmail.com>
2022-01-25 01:11:06 +0800

    first commit

---
+++ Biznet Internet Notifier.py
@@ -0,0 +1,34 @@
+from pythonping import ping
+import os
+from mutagen.mp3 import MP3
+import time
+
+
+def pinging():
+    domain = input('Input the domain to use (Default Google) : ')
+    baseline = input('Input the highest ping to wait (Default 50) : ')
+    if not domain:
+        domain = '8.8.8.8'
+    if not baseline:
+        baseline = 50
+    for sound in os.listdir('.'):
+        if sound.endswith('.mp3'):
+            mp3use = sound
+            mp3time = MP3(mp3use)
+            break
+        else:
+            print('No mp3 file to use')
+    os.system('cls')
+    while True:
+        response = ping(domain, count=5)
+        if response.rtt_avg_ms <= float(baseline):
+            os.system(mp3use)
+            time.sleep(int(mp3time.info.length))
+        else:
+            os.system('cls')
+            print('Ping is still above ' + str(baseline))
+            print(response.rtt_avg_ms)
+            
+
+
+pinging()
